86bd4fab8e93946e7603af9e0e0336427556b281
Sergey228i666i69 <cgenerozov@gmail.com>
2025-10-01 15:26:26 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -2,10 +2,28 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/web")
 def start():
     return """<doctype html> 
-    <html> 
-       <body> 
-           <h1>Web-сервер на flask</h1> 
-       </body> 
-    </html>"""
+        <html> 
+            <body> 
+                <h1>Web-сервер на flask</h1>
+                <a href="/author">author</a>  
+            </body> 
+        </html>"""
+
+@app.route("/author")
+def author():
+    name = "Генерозов Сергей Евгеньевич"
+    group = "ФБИ-33"
+    faculty = "ФБ"
+
+    return """
+        <html> 
+            <body> 
+                <p>Студент: """ + name + """</p>
+                <p>Группа: """ + group + """</p>
+                <p>Факультет: """ + faculty + """</p>
+                <a href="/web">web</a> 
+            </body> 
+        </html>"""
