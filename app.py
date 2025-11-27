eea538d1c399eed4ded9933802b4831cf8a50026
Sergey228i666i69 <cgenerozov@gmail.com>
2025-11-27 09:40:41 +0700

    Циклы в шаблоне

--- app.py
+++ app.py
@@ -447,5 +447,12 @@ def example():
     group = 'ФБИ-33'
     course = '3'
     number = '2'
-    return render_template('example.html', name=name, group=group, course=course, number=number)
+    fruits = [
+    {'name': 'яблоки',    'price': 100},
+    {'name': 'груши',     'price': 120},
+    {'name': 'апельсины', 'price': 80},
+    {'name': 'мандарины', 'price': 95},
+    {'name': 'манго',     'price': 321}
+]
+    return render_template('example.html', name=name, group=group, course=course, number=number, fruits=fruits)
 
